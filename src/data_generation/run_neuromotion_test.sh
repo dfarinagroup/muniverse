f7acdb531d6ed667793f0dbb2f98e9ada3e0cd74
pranavm19 <pranav.mamidanna@gmail.com>
2025-03-28 13:42:43 +0000

    Finalizes cpu-only docker container
    Pull docker container using: docker pull pranavm19/muniverse-test:no-entry
    For singularity, do: singularity pull muniverse-test.sif docker://pranavm19/muniverse-test:no-entry
    
    Recommended usage: singularity run --cleanenv muniverse-test.sif ./run_neuromotion_test.sh (assuming you are in the right directory, and that the bash script has execution permission - else do chmod +x run_neuromotion_test.sh).

--- src/data_generation/run_neuromotion_test.sh
+++ src/data_generation/run_neuromotion_test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e  # fail on first error
 
+# Initialize conda
+source /opt/conda/etc/profile.d/conda.sh
+
+# Activate NeuroMotion env
+conda activate NeuroMotion
+
+# Move to NeuroMotion folder
+cd /opt/NeuroMotion
+
 # Run get_data and generate EMG
 python scripts/mov2emg.py
